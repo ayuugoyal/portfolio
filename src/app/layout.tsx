4d358c8240eba946a463a7af949bb7a774d96136
ayuugoyal <ayushgoyal8178@gmail.com>
2024-08-27 00:02:53 +0530

    feat: add main page, skills, experiecnce

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
                     fontSans.variable
                 )}
             >
-                <ThemeProvider attribute="class" defaultTheme="light">
+                <ThemeProvider attribute="class" defaultTheme="dark">
                     <Navbar />
                     {children}
                 </ThemeProvider>
