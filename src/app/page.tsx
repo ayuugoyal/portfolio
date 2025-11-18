c38fc639b5927612de52624d986b2c0afda21613
Ayush Goyal <104688146+ayuugoyal@users.noreply.github.com>
2025-11-18 20:41:21 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -396,7 +396,7 @@ export default function Home() {
                             />
                             <BlurFade delay={BLUR_FADE_DELAY} className="md:text-xl space-y-2 ">
                                 <Highlighter action="underline" color="#FF9800">
-                                    DevOps Engineer
+                                    GenAI Engineer
                                 </Highlighter> &{" "}
                                 <Highlighter action="underline" color="#8BC34A">
                                     Robotics Software Engineer
